9f024a094e9fd65b04a22477f9f26861df3c7b7a
vaishaliwebvillee <vaishali.webvillee@gmail.com>
2020-11-06 12:16:42 +0530

    coupon code apply

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //SOCKET_ENDPOINT: 'https://api.orderrzz.com'
-  SOCKET_ENDPOINT: 'http://3.21.151.41:7000/'
+  SOCKET_ENDPOINT: 'https://api.orderrzz.com'
+  // SOCKET_ENDPOINT: 'http://3.21.151.41:7000/'
 
 };
 
